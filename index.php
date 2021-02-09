6d1f84d4fd1ff19c9284215a60c9baaf6b1889b1
olaulau <olaulau@gmail.com>
2021-02-09 12:27:59 +0100

    prepare to work

--- index.php
+++ index.php
@@ -5,4 +5,13 @@ $f3 = \Base::instance();
 
 $f3->config('conf/index.ini');
 
+$db_in = new DB\SQL('sqlite:'.$f3->get("db_in.host"));
+$f3->set("db_in", $db_in);
+
+$db_out = new DB\SQL(
+		'mysql:host='.$f3->get("db_out.host").';port='.$f3->get("db_out.port").';dbname='.$f3->get("db_out.schema"),
+		$f3->get("db_out.user"), $f3->get("db_out.password")
+		);
+$f3->set("db_out", $db_out);
+
 $f3->run();
