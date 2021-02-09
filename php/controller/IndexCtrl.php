2c7b8864dd9f522827bce66e02f07b7a59c2e8c3
olaulau <olaulau@gmail.com>
2021-02-09 19:43:39 +0100

    add priority page

--- php/controller/IndexCtrl.php
+++ php/controller/IndexCtrl.php
@@ -18,6 +18,13 @@ class IndexCtrl
 	
 	public static function indexGET ()
 	{
+		$view = new \View();
+		echo $view->render('index.phtml');
+	}
+	
+	
+	public static function migrateGET ()
+	{
 		$f3 = \Base::instance();
 		$db_in = $f3->get("db_in"); /* @var $db_in \DB\SQL\Mapper */
 		$db_out = $f3->get("db_out"); /* @var $db_in \DB\SQL\Mapper */
@@ -97,10 +104,99 @@ class IndexCtrl
 		}
 		
 		die;
-		
-		$view = new \View();
-		echo $view->render('index.phtml');
 	}
 	
+	
+	public static function priorityGET ()
+	{
+		$f3 = \Base::instance();
+		$db_out = $f3->get("db_out"); /* @var $db_in \DB\SQL\Mapper */
+		
+		$sql = '
+			SELECT		c.title AS étape, t.title AS titre, t.description, t.date_due as délai
+			FROM		tasks t
+				INNER JOIN	columns c ON t.column_id = c.id
+			WHERE		c.title  NOT LIKE "%FACTURATION%"
+				AND		t.date_due != ""
+			ORDER BY	t.date_due ASC, c.position ASC
+		';
+		$data = $db_out->exec($sql);
+		
+		$data2 = [];
+		foreach ($data as $id => $row) {
+			if (!empty ($row['délai'])) {
+				$date = new \DateTime();
+				$date->setTimestamp ( $row['délai'] );
+				$real_date = $date->format('Y-m-d');
+				unset($row['délai']);
+		    	if (!isset ($data2[$real_date])) {
+		    		$data2[$real_date] = [];
+		    	}
+		    	$data2[$real_date][] = $row;
+			}
+		}
+		ksort($data2);
+		
+		$months = [
+		    'en' => [
+		        'January',
+		        'February',
+		        'March',
+		        'April',
+		        'May',
+		        'June',
+		        'July',
+		        'August',
+		        'September',
+		        'October',
+		        'November',
+		        'December',
+		    ],
+		    'fr' => [
+		        'Janvier',
+		        'Février',
+		        'Mars',
+		        'Avril',
+		        'Mai',
+		        'Juin',
+		        'Juillet',
+		        'Août',
+		        'Septembre',
+		        'Octobre',
+		        'Novembre',
+		        'Décembre',
+		    ]
+		];
+		
+		$days_of_week = [
+		    'en' => [
+		        'Sunday',
+		        'Monday',
+		        'Tuesday',
+		        'Wednesday',
+		        'Thursday',
+		        'Friday',
+		        'Saturday',
+		    ],
+		    'fr' => [
+		        'Dimanche',
+		        'Lundi',
+		        'Mardi',
+		        'Mercredi',
+		        'Jeudi',
+		        'Vendredi',
+		        'Samedi',
+		    ]
+		];
+		
+		$f3->set("PAGE.title", "Planning par ordre de délai");
+		$f3->set("data2", $data2);
+		$f3->set("months", $months);
+		$f3->set("days_of_week", $days_of_week);
+		
+		
+		$view = new \View();
+		echo $view->render('priority.phtml');
+	}
 	
 }
