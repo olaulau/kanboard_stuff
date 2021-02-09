3c321c166a5b1532bbc83603d5382b112e2e355e
olaulau <olaulau@gmail.com>
2021-02-09 17:52:04 +0100

    fix default swimlane

--- php/controller/IndexCtrl.php
+++ php/controller/IndexCtrl.php
@@ -22,6 +22,16 @@ class IndexCtrl
 		$db_in = $f3->get("db_in"); /* @var $db_in \DB\SQL\Mapper */
 		$db_out = $f3->get("db_out"); /* @var $db_in \DB\SQL\Mapper */
 		
+		
+		// check db's connected
+		if(!$db_in->exists("lane")) {
+			die("input database error");
+		}
+		if(!$db_out->exists("columns")) {
+			die("output database error");
+		}
+		
+		
 		// remove  output columns
 		$out_columns_wrapper = new \DB\SQL\Mapper($db_out, "columns");
 		$out_columns = $out_columns_wrapper->find(['project_id = ?', $f3->get("kanboard.project_id")], ["order" => "position DESC"]);
@@ -42,6 +52,7 @@ class IndexCtrl
 			$lane_to_column[$in_lane->id] = $column->id;
 		}
 		
+		
 		// copy tickets
 		$in_item_wrapper = new \DB\SQL\Mapper($db_in, "item");
 		$now = time();
@@ -51,6 +62,10 @@ class IndexCtrl
 			"#ffffe0" => "yellow",
 			"#bee7f4" => "blue",
 		];
+		
+		$out_swimlane_wrapper = new \DB\SQL\Mapper($db_out, "swimlanes");
+		$default_swimlane = $out_swimlane_wrapper->findone(["project_id = ? AND position = ?", $f3->get("kanboard.project_id"), 1], []);
+		
 		foreach ($lane_to_column as  $lane_id => $column_id) {
 			$in_items = $in_item_wrapper->find(['lane_id = ?', $lane_id], ["order" => "position ASC"]);
 			foreach ($in_items as $i => $in_item) {
@@ -75,7 +90,7 @@ class IndexCtrl
 				$task->column_id = $column_id;
 				$task->position = $i+1;
 				$task->date_modification = $now;
-				$task->swimlane_id = 1; // default
+				$task->swimlane_id = $default_swimlane->id;
 				$task->date_moved = $now;
 				$task->save();
 			}
