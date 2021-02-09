4dfa66dd7a744ca5cd1f52ea5e928f083cfc3f0e
olaulau <olaulau@gmail.com>
2021-02-09 17:03:46 +0100

    copy columns and tasks

---
+++ TODO.md
@@ -0,0 +1,14 @@
+kanboard plugins : https://kanboard.org/plugins.html
+
+https://github.com/dmorlitz/kanboard-duedate
+https://github.com/manne65-hd/Kanboard-SortBoardByDates
+
+https://github.com/psy-q/kanboard-plugin-grabscroll
+
+https://github.com/kenlog/KangarooJump
+https://github.com/ipunkt/KanboardSearch
+
+https://github.com/ipunkt/KanboardOffline
+
+https://gitlab.com/taeluf/php/kanboard-essentials
+
--- php/controller/IndexCtrl.php
+++ php/controller/IndexCtrl.php
@@ -20,16 +20,68 @@ class IndexCtrl
 	{
 		$f3 = \Base::instance();
 		$db_in = $f3->get("db_in"); /* @var $db_in \DB\SQL\Mapper */
-		$db_out = $f3->get("db_out");
+		$db_out = $f3->get("db_out"); /* @var $db_in \DB\SQL\Mapper */
 		
-		$in_lane_wrapper = new \DB\SQL\Mapper($db_in, "lane");
-		$in_lanes = $in_lane_wrapper->find(['board_id = ?', 2], ["order" => "position ASC"]);
-		foreach ($in_lanes as $in_lane) {
-			echo "$in_lane->name <br/>";
+		// remove  output columns
+		$out_columns_wrapper = new \DB\SQL\Mapper($db_out, "columns");
+		$out_columns = $out_columns_wrapper->find(['project_id = ?', $f3->get("kanboard.project_id")], ["order" => "position DESC"]);
+		foreach ($out_columns as $out_column) {
+			$out_column->erase();
 		}
+		
+		// copy columns
+		$in_lane_wrapper = new \DB\SQL\Mapper($db_in, "lane");
+		$in_lanes = $in_lane_wrapper->find(['board_id = ?', $f3->get("taskboard.board_id")], ["order" => "position ASC"]);
+		$lane_to_column = [];
+		foreach ($in_lanes as $i => $in_lane) {
+			$column = new \DB\SQL\Mapper($db_out, "columns");
+			$column->title = $in_lane->name;
+			$column->position = $i+1;
+			$column->project_id = $f3->get("kanboard.project_id");
+			$column->save();
+			$lane_to_column[$in_lane->id] = $column->id;
+		}
+		
+		// copy tickets
+		$in_item_wrapper = new \DB\SQL\Mapper($db_in, "item");
+		$now = time();
+		$colors = [
+			"#c3f4b5" => "green",
+			"#ffbaba" => "red",
+			"#ffffe0" => "yellow",
+			"#bee7f4" => "blue",
+		];
+		foreach ($lane_to_column as  $lane_id => $column_id) {
+			$in_items = $in_item_wrapper->find(['lane_id = ?', $lane_id], ["order" => "position ASC"]);
+			foreach ($in_items as $i => $in_item) {
+				$task = new \DB\SQL\Mapper($db_out, "tasks");
+				$task->title = $in_item->title;
+				$task->description = $in_item->description;
+				$task->date_creation = $now;
+
+				$task->date_due = null;
+				$d = \DateTime::createFromFormat("m/d/Y", $in_item->due_date);
+				if($d !== false) {
+					$task->date_due = $d->getTimestamp();
+				}
+				
+				$task->color_id = null;
+				if( $in_item->color && !empty($colors[ $in_item->color ]) ) {
+					$color = $colors[ $in_item->color ];
+					$task->color_id = $color;
+				}
+				
+				$task->project_id = $f3->get("kanboard.project_id");
+				$task->column_id = $column_id;
+				$task->position = $i+1;
+				$task->date_modification = $now;
+				$task->swimlane_id = 1; // default
+				$task->date_moved = $now;
+				$task->save();
+			}
+		}
+		
 		die;
-		
-		
 		
 		$view = new \View();
 		echo $view->render('index.phtml');
