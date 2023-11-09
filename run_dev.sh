73a01f6daf26d86d73fc5d7a83e43a4a077aa86d
olaulau <olaulau@gmail.com>
2023-11-09 17:37:29 +0100

    forgot to add a file

--- run_dev.sh
+++ run_dev.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # cleanup destinations
-mv data/devis/done/*.csv data/devis/
-mv data/devis/error/*.csv data/devis/
-mv data/prod/done/*.csv data/prod/
-mv data/prod/error/*.csv data/prod/
+mv data/devis/done/*.csv data/devis/stock/
+mv data/devis/error/*.csv data/devis/stock/
+mv data/prod/done/*.csv data/prod/stock/
+mv data/prod/error/*.csv data/prod/stock/
 
 # copy data
 cp data/devis/stock/* data/devis/
