6b64769868b1dd8ef4295d4cd4f7d49e7104f717
olaulau <olaulau@gmail.com>
2023-11-09 18:01:40 +0100

    prepare_dev : also move non treated files to stock

--- prepare_dev.sh
+++ prepare_dev.sh
@@ -3,8 +3,10 @@
 # cleanup destinations
 mv -f data/devis/done/*.csv data/devis/stock/
 mv -f data/devis/error/*.csv data/devis/stock/
+mv -f data/devis/*.csv data/devis/stock/
 mv -f data/prod/done/*.csv data/prod/stock/
 mv -f data/prod/error/*.csv data/prod/stock/
+mv -f data/prod/*.csv data/prod/stock/
 
 # copy data
 cp data/devis/stock/* data/devis/
